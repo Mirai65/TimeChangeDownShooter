545274efc2de50d4aa3e724e67162814265736f1
Jack H Ramirez <jramirez14@fingerlakes.edu>
2022-05-06 10:51:58 -0400

    Yes

--- Assets/Scripts/Game/UIManager.cs
+++ Assets/Scripts/Game/UIManager.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
    [SerializeField] private GameObject deathPanel;
+   [SerializeField]
+   private TextMeshProUGUI _ammoTextMeshPro;
+  
 
    public void ToggleDeathPanel()
    {
       deathPanel.SetActive(!deathPanel.activeSelf);
    }
-
+   
+   public void UpdateAmmo(int count)
+   {
+      _ammoTextMeshPro.text = "Ammo: " + count;
+   }
 }
